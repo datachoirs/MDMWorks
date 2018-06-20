5416a148b68bf942f155af3891f5148a4fb9d857
datachoirs <a@a.com>
2018-06-20 11:29:09 -0400

    Adding a git asset

---
+++ ProxyServices/src/com/datachoirs/mdm/bp/constant/GitCommit.java
@@ -0,0 +1,13 @@
+package com.datachoirs.mdm.bp.constant;
+/*
+ * This class tests the git commit.
+ */
+
+public class GitCommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
